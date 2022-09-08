e39174d62727a05e5bf77a7ee31d8315c489f84a
Mahmud Asrul <mahmud.asrul@ui.ac.id>
2022-09-08 13:58:13 +0700

    Sudah Selesai Lab 01

---
+++ wishlist/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class BarangWishlist(models.Model):
+    nama_barang = models.CharField(max_length=50)
+    harga_barang = models.IntegerField()
+    deskripsi = models.TextField()
+# Create your models here.
