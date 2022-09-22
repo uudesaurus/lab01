020cdc784abe527d94238cd2b5ff7b7b1b44f7c1
Mahmud Asrul <mahmud.asrul@ui.ac.id>
2022-09-22 13:51:35 +0700

    tugas lab03 selesai

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -4,6 +4,9 @@ from wishlist.views import menerima_fungsi
 from wishlist.views import menerima_fungsi_json
 from wishlist.views import menerima_request_id_json
 from wishlist.views import menerima_request_id_xml
+from wishlist.views import register
+from wishlist.views import login_user
+from wishlist.views import logout_user
 
 app_name = 'wishlist'
 
@@ -13,4 +16,7 @@ urlpatterns = [
     path('json/', menerima_fungsi_json, name='menerima_fungsi_json'),
     path('json/<int:id>', menerima_request_id_json, name='menerima_request_id_json'),
     path('json/<int:id>', menerima_request_id_xml, name='menerima_request_id_xml'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'), 
 ]
